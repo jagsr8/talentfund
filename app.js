3bdf82e01c297f7065ba112bab832722f550927f
Ganapathy Hari Narayan <ganapathy.hari.narayan@gatech.edu>
2015-11-07 21:23:08 -0500

    Added routes to the backend API; Added Schemas.

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var Student = require('./models/Student');
+var Sponsor = require('./models/Sponsor');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -20,11 +23,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('less-middleware')(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/talendFund');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,59 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var router = express.Router();
+var Students = mongoose.model('Student')
+var Sponsors = mongoose.model('Sponsor');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/student/:id', function(req, res, next){
+  Students.findById(req.params.id, function(err, get){
+    if (err) {
+      return next(err);
+    }
+    res.json(get);
+  });
+});
+
+router.get('/student', function(req, res, next){
+  Students.find(function(err, get){
+    if (err) {
+      return next(err);
+    }
+    res.json(get);
+  });
+});
+
+router.post('/student', function(req, res, next){
+  Students.create(req.body, function(err, post){
+    if (err) {
+      return next(err);
+    }
+    res.json({ message: 'Bear created!' });
+    res.json(post);
+  });
+
+});
+
+router.get('/sponsors/:id', function(req, res, next){
+  Sponsors.findById(req.params.id, function(err, get){
+    if (err) {
+      return next(err);
+    }
+    res.json(get);
+  });
+});
+
+router.post('/sponsors', function(req, res, next){
+  Sponsors.create(req.body, function(err, post){
+    if (err) {
+      return next(err);
+    }
+    res.json(post);
+  });
 });
 
 module.exports = router;
